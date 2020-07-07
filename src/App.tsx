583afe4e6cae3bde43ca3566d52fee9d19cb10d9
Cezary Dmowski <cezdmo@gmail.com>
2020-07-07 21:29:33 +0200

    refactored Counter into BetterCounter to show * how to use function based components (instead of clases) * how to use react Hooks to make code more readeable

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import Login from './Components/Login'
-import { Counter } from './Components/Counter'
+import Login from './Components/Login';
+import { Counter } from './Components/Counter';
+import BetterCounter from './Components/BetterCounter';
 
 function App() {
   return (
     <div className="App">
       <div className="container-xl">
         <nav className="navbar navbar-dark bg-primary">
-            <h3>Hello World :D</h3>
-            <Login />
-        </nav>        
+          <h3>Hello World :D</h3>
+          <Login />
+        </nav>
         <Counter />
+        <hr></hr>
+        <BetterCounter />
       </div>
     </div>
   );
