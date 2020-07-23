c2418a83953e5e1ff28ba61b5807fa697b0807fb
Patryk <patryk.banski@gmail.com>
2020-07-23 19:33:50 +0200

    add clue

--- src/Components/YotubeBox/YoutubeBox.tsx
+++ src/Components/YotubeBox/YoutubeBox.tsx
@@ -1,15 +1,42 @@
 import React from "react";
-import ReactPlayer, { ReactPlayerProps } from "react-player";
-import getCurrenTime from "react-player";
+import ReactPlayer from "react-player";
 import { useState } from "react";
 
+type timeStamp = {
+  time: number;
+  numberOfCommits: number;
+  comment: string;
+  userId: number;
+};
+
 export default () => {
-  const [time, setTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
   const [playing, setPlaying] = useState(true);
   const [volume, setVolume] = useState(1);
-  const [muted, setMuted] = useState(true);
+  const [muted, setMuted] = useState(false);
 
+  const [timeStamps, setTimeStamps] = useState<timeStamp[]>([]);
+  const CLUE_DURATION = 15;
   const ref = React.createRef<ReactPlayer>();
+
+  const addClue = () => {
+    setTimeStamps(
+      [
+        ...timeStamps,
+        { time: currentTime, numberOfCommits: 1, comment: "test", userId: 123 },
+      ].sort((a, b) => a.time - b.time)
+    );
+  };
+
+  const revMovie = () => {
+    ref.current?.seekTo(Math.max(currentTime - 15, 0));
+  };
+
+  const isNearClue = () => {
+    return timeStamps.filter(
+      (timeStamp) => Math.abs(currentTime - timeStamp.time) < CLUE_DURATION / 2
+    ).length;
+  };
 
   return (
     <>
@@ -24,15 +51,29 @@ export default () => {
             light={false}
             loop={false}
             volume={volume}
-            muted={false}
-            progressInterval={100}            
+            muted={muted}
+            progressInterval={100}
             onProgress={() => {
               const time = ref.current?.getCurrentTime();
-              if (time !== undefined) setTime(time)
+              if (time !== undefined) setCurrentTime(time);
             }}
           />
         </div>
-        time: {time} s
+        time: {currentTime}
+        <br />
+        {isNearClue() > 0 ? "A Clue is now?" : ""}
+        <br />
+        <button onClick={addClue}>Clue!</button>
+        <button onClick={revMovie}>Rev!</button>
+        <br />
+        <br />
+        <div>
+          <ol>
+            {timeStamps.map((stamp) => (
+              <li>{stamp.time}</li>
+            ))}
+          </ol>
+        </div>
       </div>
     </>
   );
